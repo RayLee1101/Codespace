de5da286da41e8b58529fb1da0db51c55774e46c
RayLee <101978461+RayLee1101@users.noreply.github.com>
2024-10-28 02:47:23 +0000

    20241028_1

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # git status 檢查所有檔案的狀態
 # git add . 將所有檔案上傳到暫存區
 # git commit -m "" 提交檔案 "動作名稱"
-# git push origin <分支名稱> 將變更上傳到github <> 不用打
+# git push origin <分支名稱> 將變更上傳到github <> 不用打. --force 強制上傳
--- hw18.py
+++ hw18.py
@@ -1,9 +1,9 @@
 lists = list(map(lambda x: int(x), input().split()))
 total = int(input())
-max = 0
+max = -1
 result = []
-for i in range (len(lists)):
-    for j in range(i, len(lists)):
+for i in range (len(lists)-1):
+    for j in range(i+1, len(lists)):
         if lists[i] + lists[j] == total and i * j > max:
             result = [i, j]
             max = i*j
