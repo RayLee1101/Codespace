cef09a1fdd55596d265538a05c4700374c3ecc3d
RayLee <101978461+RayLee1101@users.noreply.github.com>
2024-10-29 05:19:22 +0000

    20241029_2

--- modify/1_2.py
+++ modify/1_2.py
@@ -1,23 +1,21 @@
 def input_data():
-    n = int(input())
     course = []
-    for i in range(n):
+    for i in range(int(input())):
         course.append([int(input()),[]])
         for j in range(int(input())):
             course[i][1].append(int(input("time:")))
-    check_conflict(n, course)
+    check_conflict(len(course), course)
 
 def check_conflict(n, course):
     for i in range(n-1):
         for j in range(i + 1, n):
             check(course[i], course[j])
-  #          for x in range(len(course[x][1])):
-        #        if course[i][1][x] in course[j][1]:
-           #         print(f"{course[i][0]} and {course[j][0]} conflict on {course[i][1][x]}")
+
 def check(val1, val2):
     for x in range(len(val1[1])):
         if val1[1][x] in val2[1]:
             print(f"{val1[0]} and {val2[0]} onflict on {val1[1][x]}")
+            
 def main():
     input_data()
 
